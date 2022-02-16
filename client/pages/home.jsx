621764f342862027e0347591fdef2f66f315970c
vkahlon <vickram.kahlon@gmail.com>
2022-02-15 21:49:19 -0800

    Finished NavBar

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
-    <img src="../../server/public/images/ox.png" alt="test" />
+    <img src="./images/ox.png" alt="test" />
     </>
   );
 }
