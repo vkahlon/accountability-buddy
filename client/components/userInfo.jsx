be3341908064450fddacd97105c924165b2c8629
vkahlon <vickram.kahlon@gmail.com>
2022-02-22 20:28:27 -0800

    Fixed fetch, Data loads sequentially

--- client/components/userInfo.jsx
+++ client/components/userInfo.jsx
@@ -18,49 +18,53 @@ export default class UserInfo extends React.Component {
           mealList.push({ id: idString, content: mealData[i].mealName, calories: -mealData[i].calories, icon: './images/fork.png' });
         }
         return mealList;
-      });
-    fetch('api/exercises')
-      .then(response => response.json())
-      .then(data => {
-        const exerciseData = data;
-        for (let i = 0; i < exerciseData.length; i++) {
-          const idString = (`exercise-${exerciseData[i].exerciseId}`);
-          exerciseList.push({ id: idString, content: exerciseData[i].exerciseName, calories: exerciseData[i].calories, icon: './images/dumbell.png' });
-        }
-        return exerciseList;
+      })
+      .then(meal => {
+        fetch('api/exercises')
+          .then(response => response.json())
+          .then(data => {
+            const exerciseData = data;
+            for (let i = 0; i < exerciseData.length; i++) {
+              const idString = (`exercise-${exerciseData[i].exerciseId}`);
+              exerciseList.push({ id: idString, content: exerciseData[i].exerciseName, calories: exerciseData[i].calories, icon: './images/dumbell.png' });
+            }
+            return exerciseList;
+          })
+          .then(exercise => {
+            fetch('api/user')
+              .then(response => response.json())
+              .then(data => {
+                const calorie = data;
+                this.setState({
+                  data: {
+                    columns: {
+                      'column-1': {
+                        id: 'column-1',
+                        title: 'Exercises',
+                        taskIds: exerciseList
+                      },
+                      'column-2': {
+                        id: 'column-2',
+                        title: 'Meals',
+                        taskIds: mealList
+                      },
+                      'column-3': {
+                        id: 'column-3',
+                        title: 'Today',
+                        taskIds: []
+                      }
+                    },
+                    columnOrder: ['column-1', 'column-2', 'column-3'],
+                    dailyCalorie: calorie[0].dailyCalorie
+                  },
+                  isLoading: false,
+                  reserveCalorie: calorie[0].dailyCalorie
+                }
+                );
+              });
+          });
       });
 
-    fetch('api/user')
-      .then(response => response.json())
-      .then(data => {
-        const calorie = data;
-        return this.setState({
-          data: {
-            columns: {
-              'column-1': {
-                id: 'column-1',
-                title: 'Exercises',
-                taskIds: exerciseList
-              },
-              'column-2': {
-                id: 'column-2',
-                title: 'Meals',
-                taskIds: mealList
-              },
-              'column-3': {
-                id: 'column-3',
-                title: 'Today',
-                taskIds: []
-              }
-            },
-            columnOrder: ['column-1', 'column-2', 'column-3'],
-            dailyCalorie: calorie[0].dailyCalorie
-          },
-          isLoading: false,
-          reserveCalorie: calorie[0].dailyCalorie
-        }
-        );
-      });
   }
 
   render() {
