6332195ad22205414bffd939c39e6f5710a47eaa
Vickram Kahlon <vickram.kahlon@gmail.org>
2022-03-09 20:41:01 +0000

    Patched Navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 export default function Navbar(props) {
-  let isSigned = <a className="nav-link mr-4" href="#sign-in" style={{ color: '#007BFF' }}><span data-toggle="collapse" data-target="#navbarSupportedContent">Sign In</span></a>;
+  let isSigned = <a className="nav-link mr-4" href="#sign-in"><span data-toggle="collapse" data-target="#navbarSupportedContent">Sign In</span></a>;
   let featurePages = null;
   if (props.token) {
-    isSigned = <a onClick={props.onSignOut} className="nav-link mr-4" href="#" style={{ color: '#007BFF' }}>Sign Out</a>;
+    isSigned = <a onClick={props.onSignOut} className="nav-link mr-4" href="#">Sign Out</a>;
     featurePages = <ul className="navbar-nav mr-auto" data-toggle="collapse" data-target="#navbarSupportedContent">
       <li className="nav-item active">
-        <a className="nav-link" href="#calculator" style={{ color: '#007BFF' }}><span>Calculator</span></a>
+        <a className="nav-link" href="#calculator"><span>Calculator</span></a>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="#meals" style={{ color: '#007BFF' }}><span>Add Meal</span></a>
+        <a className="nav-link" href="#meals"><span>Add Meal</span></a>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="#exercises" style={{ color: '#007BFF' }}><span>Add Exercise</span></a>
+        <a className="nav-link" href="#exercises"><span>Add Exercise</span></a>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="#codex" style={{ color: '#007BFF' }}><span>Codex</span></a>
+        <a className="nav-link" href="#codex"><span>Codex</span></a>
       </li>
       <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" style={{ color: '#007BFF' }} role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a className="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Edit
         </a>
         <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item bg-dark" style={{ color: '#007BFF' }} href="#edit-meal"><span>Meal</span></a>
-          <a className="dropdown-item bg-dark" style={{ color: '#007BFF' }} href="#edit-exercise"><span>Exercise</span></a>
+          <a className="dropdown-item bg-dark text-light" href="#edit-meal"><span>Meal</span></a>
+          <a className="dropdown-item bg-dark text-light" href="#edit-exercise"><span>Exercise</span></a>
         </div>
       </li>
     </ul>;
@@ -39,7 +39,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav d-flex justify-content-end">
             <li className="nav-item active d-flex justify-content-end">
               {isSigned}
-            <a className="nav-link" href="#register" style={{ color: '#007BFF' }}><span data-toggle="collapse" data-target="#navbarSupportedContent">Register</span></a>
+            <a className="nav-link" href="#register"><span data-toggle="collapse" data-target="#navbarSupportedContent">Register</span></a>
             </li>
           </ul>
         </div>
