cdd656e97ab53666836051db12d5e59587550179
vkahlon <vickram.kahlon@gmail.com>
2022-03-09 21:36:31 -0800

    Ox becomes transparent

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
 <>
       <div className='container-lg'>
          <div className='d-flex justify-content-center mr-5'>
-            <img className='mr-2'src="./images/ox1.png" alt="test" style={{ width: '130px' }} />
+            <img className='mr-2'src="./images/ox.png" alt="test" style={{ width: '130px' }} />
             <h2 className='mt-5 mr-5'>{props.header}</h2>
           </div>
       </div>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home(props) {
     <div className='container-lg'>
         <div className='row'>
         <div className='col-5 col-lg-5 d-flex justify-content-end'>
-          <img src="./images/ox1.png" alt="test" style={{ width: '150px' }} />
+          <img src="./images/ox.png" alt="test" style={{ width: '150px' }} />
       </div>
       <div className='col-6 col-lg-6'>
             <h2 className="mt-4">Welcome!</h2>
